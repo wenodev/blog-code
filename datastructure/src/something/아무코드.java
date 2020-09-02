8a3841ee4186bb4076318cabb98095ad96c49fb9
logicstark <logicalstark1105@gmail.com>
2020-09-02 18:27:56 +0900

    더맵게 수정

--- datastructure/src/something/아무코드.java
+++ datastructure/src/something/아무코드.java
@@ -4,66 +4,47 @@ import java.util.*;
 
 public class 아무코드 {
     public static void main(String[] args) {
-//        int[] progresses = {93,30,55};
-//        int[] speeds = {1,30,5};
 
-//        int[] progresses = {55, 93, 30, 55};
-//        int[] speeds = {5, 1, 30, 5};
+        int[] scoville = new int[]{1, 2, 3, 9, 10, 12};
+        int K = 7;
 
-                int[] progresses = {93,30,55,30};
-        int[] speeds = {1,30,5,30};
-
-        Solution(progresses, speeds);
+        System.out.println(Solution(scoville, K));
 
     }
 
-    public static void Solution(int[] progresses, int[] speeds) {
+    public static int Solution(int[] scoville, int K) {
 
-        Stack<Integer> stack = new Stack<>();
-        List<Integer> answerList = new ArrayList<>();
+        int answer = 0;
+        int temp = 0;
 
-        for (int i = progresses.length-1; i >= 0; i--) {
-            int temp = (100 - progresses[i]) / speeds[i];
-            if ((100 - progresses[i]) % speeds[i] != 0) {
-                temp++;
-            }
-            System.out.println("temp : " + temp);
-            stack.push(temp);
+        List<Integer> scovilleList = new ArrayList<>();
+        for (int num : scoville) {
+            scovilleList.add(num);
         }
 
-        int count=0;
-        int temp = stack.peek();
-        for(int i=0; i<progresses.length; i++){
 
-            stack.pop();
-            count++;
+        do {
 
-            if(!stack.empty()){
-                if (temp < stack.peek()){
-                    temp = stack.peek();
-                    answerList.add(count);
-                    count=0;
+            if (scovilleList.get(0) >= K) {
+                break;
+            }
+
+            temp = scovilleList.remove(0) + (scovilleList.remove(0) * 2);
+            for (int j = 0; j < scovilleList.size(); j++) {
+                if (temp < scovilleList.get(j)) {
+                    scovilleList.add(j, temp);
+                    break;
+                }
+                if (temp > scovilleList.get(scovilleList.size() - 1)) {
+                    scovilleList.add(temp);
+                    break;
                 }
             }
+            answer++;
+        }while (scovilleList.size() != 2);
 
-        }
-        answerList.add(count);
-
-        int[] array = new int[answerList.size()];
-
-        for(int i=0; i<array.length; i++){
-            array[i] = answerList.get(i);
-        }
-
-
-        for(int num : array){
-            System.out.println(num);
-        }
-
-
-
-
-
-
+        return answer;
     }
+
+
 }
